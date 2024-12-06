747b6281f957e47c50440c9b2915c65332c5c84b
Dale Carman <dale@mrgroovejones.com>
2024-12-05 18:55:06 -0600

    lots of updates. added transition to blood vessel scene

---
+++ SpawnAndAttrack/Views/BloodVesselView.swift
@@ -0,0 +1,18 @@
+//
+//  BloodVesselView.swift
+//  SpawnAndAttrack
+//
+//  Created by Dale Carman on 12/5/24.
+//
+
+import SwiftUI
+
+struct BloodVesselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BloodVesselView()
+}
